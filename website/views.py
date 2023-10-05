9211c88686e053c1686b390343aded3cf9097edf
JustPC <robert49324@gmail.com>
2023-10-05 23:10:29 +0300

    Added product page

--- website/views.py
+++ website/views.py
@@ -43,6 +43,11 @@ def catalog():
             return render_template("catalog.html",products=data, user=current_user,title="Блоки питания")
     return render_template("catalog.html", user=current_user, title="Каталог")
     
+@views.route("/product", methods=['GET','POST'])
+def product():
+    print(request.form["product"])
+    return render_template("product.html",user=current_user) 
+    
 @views.route("/cart", methods=['GET','POST'])
 def cart():
     return render_template("cart.html",user=current_user)
